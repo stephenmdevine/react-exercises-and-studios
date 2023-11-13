3001df4a2da9e21a926eebda051ec99c052b5253
stephenmdevine <stephenmdevine@gmail.com>
2023-11-13 15:32:48 -0600

    Part one done!

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "BOOKS! BOOKS! BOOKS!";
+   let book1 = "https://upload.wikimedia.org/wikipedia/en/d/de/Dune-Frank_Herbert_%281965%29_First_edition.jpg";
+   let book2 = "https://upload.wikimedia.org/wikipedia/en/3/3b/The_Atlas_Six.jpg";
+   let book3 = "https://upload.wikimedia.org/wikipedia/commons/d/d6/Louis_Fran%C3%A7ais-Dant%C3%A8s_sur_son_rocher.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Dune by Frank Herbert" />
+         <img src={book2} alt="The Atlas Six by Olivie Blake" />
+         <img src={book3} alt="The Count of Monte Cristo by Alexander Dumas" />
       </div>      
    );
 }
